521a4eb5272b71375a5559902862022673fac629
Dillan <dillanroby@yahoo.com>
2020-10-05 00:50:40 -0700

    added script.js for theme changer, site is fully functional. Add Polish and host.

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("it's working");
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+const themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i = 0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = "style.css"
+    }
+
+    if(mode =='dark'){
+        document.getElementById('theme-style').href = 'dark.css'
+    }
+
+    if(mode =='orange'){
+        document.getElementById('theme-style').href = 'orange.css'
+    }
+
+    if(mode =='purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
